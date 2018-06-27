5cd159a3f29c4f207708633d04d4d81ade68f7e8
github username <github email address>
2018-06-27 02:26:49 -0400

    created separate css file for navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar'
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
-import '../styles/App.css'
 
 const App = (props) =>
   <Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import '../styles/App.css'
 
 const Navbar = () =>
   <div>
