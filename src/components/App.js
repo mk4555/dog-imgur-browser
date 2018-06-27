ab099a9a3d88946893462ff1f784d4c9e0375776
github username <github email address>
2018-06-27 02:22:29 -0400

    moved Navlinks into Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
+import Navbar from './Navbar'
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
-// import './App.css';
+import '../styles/App.css'
 
 const App = (props) =>
   <Router>
     <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to='/random'>Random</NavLink>
-      <NavLink to='/favorites'>Favorites</NavLink>
-      <Route exact path="/" render={() => <div>Welcome to Imgur Browser</div>} />
+      <Navbar />
+      <Route className="navbar" exact path="/" render={() => <div>Welcome to Imgur Browser</div>} />
       <Route path='/random' render={() => <div>Mock Random esting</div>} />
       <Route path='/favorites' render={() => <div>Mock Favorites </div>} />
     </div>
   </Router>;
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+
+const Navbar = () =>
+  <div>
+    <NavLink className="navbar" to="/">Home</NavLink>
+    <NavLink className="navbar" to='/random'>Random</NavLink>
+    <NavLink className="navbar" to='/favorites'>Favorites</NavLink>
+  </div>
+
+export default Navbar
