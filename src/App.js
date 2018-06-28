5933618c531ac6d16da3075c4f3e0382c1fa0c3b
github username <github email address>
2018-06-28 14:19:13 -0400

    custom css for nav deleted and used react-bootstrap for navbar

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CustomNavBar from './components/CustomNavBar'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <CustomNavBar />
+          <Route exact path='/' render={() => <div>Hello</div>} />
+          <Route path='/random' render={() => <div>Random</div>} />
+          <Route path='/favorites' render={() => <div>Favorites Sdfsadf</div>} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/CustomNavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+const CustomNavBar = () =>
+  <Navbar default collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to='/'>Imgur-Browser</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Nav>
+      <NavItem componentClass={Link} eventKey={1} href="/" to="/">
+        Home
+      </NavItem>
+      <NavItem componentClass={Link} eventKey={2} href="/random" to="/random">
+        Random
+      </NavItem>
+      <NavItem componentClass={Link} eventKey={3} href="/favorites" to="/favorites">
+        Favorites
+      </NavItem>
+    </Nav>
+  </Navbar>;
+
+export default CustomNavBar;
