1c4aef82d32a45b43d210f43d2be74c33165e2f4
Min Suk Kim <minkimcodes@gmail.com>
2018-06-28 19:09:17 -0400

    images list component added

---
+++ src/components/ImagesList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+
+
+const ImagesList = () =>
+    <Row>
+      <Col xs={6} md={4}>
+        <h2>Filler</h2>
+      </Col>
+    </Row>;
+
+
+export default ImagesList;
