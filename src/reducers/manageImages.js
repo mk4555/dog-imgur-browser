a77db6272a2599c2207c63bb8b93d790f29fdd12
Min Suk Kim <minkimcodes@gmail.com>
2018-06-29 03:17:18 -0400

    images reducer added

---
+++ src/reducers/manageImages.js
@@ -0,0 +1,12 @@
+export default function manageImages (state = {
+  images: []
+}, action) {
+  switch (action.type) {
+    case 'LOAD_IMAGES':
+      return Object.assign({}, state, {
+        images: state.images.concat(action.image)
+      });
+    default:
+      return state;
+  }
+};
