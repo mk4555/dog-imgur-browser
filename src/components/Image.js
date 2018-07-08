1dd136233452c9d172454439c2d67271b1ad1f90
Min Suk Kim <minkimcodes@gmail.com>
2018-07-08 19:44:03 -0400

    refactored image into separate class for images list

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,35 +3,16 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import fetch from 'isomorphic-fetch';
 
 class Image extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     images: []
-  //   }
-  // }
-  // componentWillMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/photos')
-  //   .then(res => res.json())
-  //   .then(data => this.setState({images: data}));
-  //
-  // }
   render(){
-    const imgs = this.state.images.map(image => (
-      <Col key ={image.id} xs={6} md={4}>
-        <h3>image.title</h3>
-        <img src={image.url} />
-      </Col>
-    ));
-    const mock = imgs.slice(0,9);
     return(
       <div>
-        {mock}
+        <Col key={this.props.image.id} xs={6} md={4}>
+          <h3>{this.props.image.title}</h3>
+          <img src={this.props.image.link} alt='Imgur' />
+        </Col>
       </div>
     )
   }
 }
 
 export default Image;
-// <Col xs={6} md={4}>
-//   <h2>Filler</h2>
-// </Col>;
--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -3,18 +3,13 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import Image from './Image'
 
 class ImagesList extends React.Component {
-
   render() {
     const imgs = this.props.images.data.map(image => (
-      <Col key ={image.id} xs={6} md={4}>
-        <h3>{image.title}</h3>
-        <img src={image.link} alt="Image"/>
-      </Col>
+      <Image image={image} />
     ));
-    const mock = imgs.slice(0,9);
     return (
       <Row>
-        {mock}
+        {imgs}
       </Row>
     )
   }
